0972a11c746eb90218cde5357a23a67058313e02
Inna Ianko <inna.i@mate.academy>
2025-05-26 14:02:30 +0300

    Minor style fix

--- tests/assertCustomerCorrectLogout.spec.js
+++ tests/assertCustomerCorrectLogout.spec.js
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Assert correct customer Logout ', async ({ page }) => {
-/* 
-Test:
-1. Open Wizard bank link https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login 
-2. Click [Customer Login]
-3. Select Neville Longbottom
-4. Click [Login]
-5. Click [Logout]
-6. Wait for the page URL https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer
-7. Assert the drop-down is present with empty value 
-*/
-
+test('Assert correct customer Logout', async ({ page }) => {
+  /* 
+  Test:
+  1. Open Wizard bank link
+    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login 
+  2. Click [Customer Login]
+  3. Select Neville Longbottom
+  4. Click [Login]
+  5. Click [Logout]
+  6. Wait for the page URL
+    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/customer
+  7. Assert the drop-down is present with empty value 
+  */
 });
